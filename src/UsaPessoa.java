e63943fc221af0fc2e1c8cbe76f0f4dfeedbc21f
13267 <13267@enautica.pt>
2021-10-29 14:30:54 +0100

    exericio 2 finished

--- src/UsaPessoa.java
+++ src/UsaPessoa.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class UsaPessoa {
     // construtor default para objetos da classe
     // UsaPessoa este construtor existe mesmo se não
     // for explicitamente declarado
     public UsaPessoa() {}
     public static void main(String[] args){
+        String nome;
+        int ida;
+        char gen;
         //Declara objeto pessoa1
         Pessoa pessoa1;
         Pessoa pessoa2;
         //Cria o objeto pessoa1 que é instância da classe
         pessoa1 = new Pessoa();
-        pessoa2 = new Pessoa("Joao",20,'M');
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("nome da pessoa2? ");
+        nome = entrada.nextLine();
+        System.out.println("idade da pessoa2? ");
+        ida = entrada.nextInt();
+        System.out.println("género da pessoa2? ");
+        gen = entrada.next().charAt(0);
+        pessoa2 = new Pessoa(nome, ida, gen);
         //Define os valores para os atributos de pessoa1
         pessoa1.setNome("Camila");
         pessoa1.setGenero('F');
